6ce9435d9cb2a7605a4ea6fdddfdf7ca279fa489
rdpi <ryanpitts95@gmail.com>
2019-01-04 22:54:22 -0400

    new API endpoints

--- routes/index.js
+++ routes/index.js
@@ -6,25 +6,25 @@ const boardController = require('../controllers/boardController');
 const indexController = require('../controllers/indexController');
 
 /* GET home page. */
-router.get('/', indexController.get_boards);
+router.get('/api/boards', indexController.get_boards);
 
 /* Create new board */
-router.post('/', indexController.create_board);
+router.post('/api/boards', indexController.create_board);
 
 // THREAD ROUTERS //
 
 // GET request for thread
-router.get('/:boardid/thread/:threadid', threadController.thread_detail);
+router.get('/api/:boardid/thread/:threadid', threadController.thread_detail);
 
 // POST request for thread
-router.post('/:boardid/thread/:threadid', threadController.reply_create_post);
+router.post('/api/:boardid/thread/:threadid', threadController.reply_create_post);
 
 // BOARD ROUTERS //
 
 // GET request for thread
-router.get('/:boardid', boardController.get_threads);
+router.get('/api/:boardid', boardController.get_threads);
 
 // POST request for thread
-router.post('/:boardid', boardController.create_thread);
+router.post('/api/:boardid', boardController.create_thread);
 
 module.exports = router;
